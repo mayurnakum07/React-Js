753cea2d717f0349e735067a87ae0fde02461c10
[username] <[user>
2023-10-12 12:27:32 +0530

    add use state hook

--- src/Include.jsx
+++ src/Include.jsx
@@ -1,90 +1,126 @@
 import React from "react";
 import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 function Include() {
-  const [name, setName] = useState({});
-  const [area, setArea] = useState(
-    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores officia incidunt eos laudantium labore! Esse reiciendis sed illum inventore pariatur maxime non debitis ad ex cupiditate, quasi, deleniti fugit repellat!"
-  );
-  const [select, setSelect] = useState("Volvo");
-
-  const names = [
-    { id: 1, name: "jorn Doe" },
-    { id: 2, name: "sara Shon" },
-    { id: 3, name: "sally velion" },
-  ];
-  const filterList = names.filter((ele3) => ele3.name.includes("jorn Doe"));
-  const filtered = filterList.map((ele4) => <li key={ele4.id}>{ele4.name}</li>);
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setName((values) => ({ ...values, [name]: value }));
+  const [Text, setText] = useState("Hello");
+  const [Check, setCheck] = useState(true);
+  const [Name, setName] = useState("Jorn");
+  const [Age, setAge] = useState(18);
+  const [Number, setNumber] = useState(12);
+  const [form, setForm] = useState({
+    firstName: "Jorn",
+    lastName: "Doe",
+    email: "jorn12@gmail.com",
+    img: "https://files.superworks.com/profileImages/iBr41Pc9u2.jpeg",
+  });
+  const inputChange = (e) => {
+    setText(e.target.value);
   };
 
-  const handleSubmit = (event) => {
-    console.log("name", name);
-    event.preventDefault();
-    alert(
-      `The name you entered has : ${name.name} and your age is : ${name.age}`
-    );
+  const chenkChange = (e) => {
+    setCheck(e.target.checked);
+  };
+  const nNumber = (n) => {
+    setNumber((n) => n + 1);
   };
 
-  const nhandle = (event) => {
-    setArea(event.target.value);
-    console.log("nHandle");
-    console.log("textInput", area);
-  };
-
-  const sHandle = (event) => {
-    setSelect(event.target.value);
-    console.log("Select", select);
-  };
   return (
-    <div>
-      <h3 className="mt-5">{filtered}</h3>
-
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter Your Name:
-          <input
-            type="text"
-            name="name"
-            value={name.userName}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Enter Your Age:
-          <input
-            type="number"
-            name="age"
-            value={name.age || ""}
-            onChange={handleChange}
-          />
-        </label>
-        <button type="submit">Submit</button> <br /> <br />
-        <label>
-          Text-area
-          <textarea
-            name="text"
-            value={area}
-            id=""
-            cols="30"
-            rows="3"
-            onChange={nhandle}
-          ></textarea>
-        </label>
-        <br /> <br />
-        <label>
-          Select Favorite Car
-          <select name="select" value={select} onChange={sHandle}>
-            <option value="Ford">Ford</option>
-            <option value="Volvo">Volvo</option>
-            <option value="Supra">Supra</option>
-          </select>
-        </label>
-      </form>
+    <div className="mt-5">
+      <Form.Control type="text" value={Text} onChange={inputChange} />
+      <h3>You Typed is : {Text} </h3>
+      <Button onClick={() => setText("hello")}>Reset</Button> <br /> <br />
+      <label>
+        <input type="checkbox" checked={Check} onChange={chenkChange} />
+        Check This Box
+      </label>
+      <h3>You {Check ? "Check" : "UnCheck"} Box</h3>
+      <br />
+      <Form.Control
+        type="text"
+        value={Name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Button className="m-3" onClick={() => setAge(Age + 1)}>
+        Increment
+      </Button>
+      <h3>
+        Hello {Name} . Your Age is {Age}
+      </h3>
+      <br />
+      <br />
+      <h3>Your Num. : {Number}</h3>
+      <Button
+        className="m-3"
+        onClick={() => {
+          nNumber();
+          nNumber();
+          nNumber();
+        }}
+      >
+        +3
+      </Button>
+      <Button className="m-3" onClick={() => nNumber()}>
+        +1
+      </Button>
+      <br />
+      <br />
+      <label>First Name :</label>{" "}
+      <input
+        type="text"
+        value={form.firstName}
+        onChange={(e) => {
+          setForm({
+            ...form,
+            firstName: e.target.value,
+          });
+        }}
+      />{" "}
+      <br /> <br />
+      <label>Last Name :</label>{" "}
+      <input
+        type="text"
+        value={form.lastName}
+        onChange={(e) => {
+          setForm({
+            ...form,
+            lastName: e.target.value,
+          });
+        }}
+      />{" "}
+      <br /> <br />
+      <label>Email :</label>{" "}
+      <input
+        type="email"
+        value={form.email}
+        onChange={(e) => {
+          setForm({
+            ...form,
+            email: e.target.value,
+          });
+        }}
+      />{" "}
+      <br /> <br />
+      <label>Img : </label>
+      <input
+        type="text"
+        value={form.img}
+        onChange={(e) => {
+          setForm({
+            ...form,
+            img: e.target.value,
+          });
+        }}
+      />
+      <h3>
+        First Name : {form.firstName}
+        <br />
+        Last Name : {form.lastName} <br />
+        Email : {form.email}
+        <br />
+        Img : <img src={form.img} alt="" />
+      </h3>
+      <br />
     </div>
   );
 }
